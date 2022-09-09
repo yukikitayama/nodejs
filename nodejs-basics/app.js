e7448d4f662eece145d8f3f5353a8b438106515b
Yuki Kitayama <yk2797@columbia.edu>
2022-09-08 17:22:02 -0700

    finished the nodejs basics

--- nodejs-basics/app.js
+++ nodejs-basics/app.js
@@ -1,61 +1,16 @@
 const http = require("http");
-const fs = require("fs");
+
+const routes = require('./routes');
 
 // function rqListener(req, res) {
-
 // };
 // http.createServer(rqListener);
-
 // http.createServer(function(req, res){
-
 // });
 
-const server = http.createServer((req, res) => {
-  // console.log(req.url, req.method, req.headers);
-  // process.exit();
+console.log(routes.someText);
 
-  const url = req.url;
-  const method = req.method;
-
-  if (url === "/") {
-    res.write("<html>");
-    res.write("<head><title>Enter Message</title></head>");
-    res.write(
-      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
-    );
-    res.write("</html>");
-
-    // Quit function execution
-    return res.end();
-  }
-
-  if (url === "/message" && method === "POST") {
-    const body = [];
-    req.on("data", (chunk) => {
-      console.log(chunk);
-      body.push(chunk);
-    });
-    return req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      // console.log(parsedBody);
-      const message = parsedBody.split("=")[1];
-      fs.writeFile("message.text", message, (err) => {
-        res.statusCode = 302;
-        res.setHeader("Location", "/");
-        return res.end();
-      });
-    });
-  }
-
-  res.setHeader("Content-Type", "text/html");
-  // Write some data to response
-  res.write("<html>");
-  res.write("<head><title>My First Page</title></head>");
-  res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
-  res.write("</html>");
-
-  // Once we are done creating response.
-  res.end();
-});
+// const server = http.createServer(routes);
+const server = http.createServer(routes.handler);
 
 server.listen(3000);
