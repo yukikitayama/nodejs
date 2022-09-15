fff9c704be9107f5832c0b409df11fd1554842f0
Yuki Kitayama <yk2797@columbia.edu>
2022-09-14 21:31:04 -0700

    still learning express.js

--- expressjs/app.js
+++ expressjs/app.js
@@ -1,26 +1,19 @@
-// const http = require('http');
-
 const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 // To extract body data from request.
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
-  );
-});
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
 
-app.use("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  res.send("<h1>Hello from Express!</h1>");
+app.use((req, res, next) => {
+  res.status(404).send('<h1>Page not found</h1>');
 });
 
 // const server = http.createServer(app);
