29b8a1f70d4d24a8640e15da3880904caaf8b380
Yuki Kitayama <yk2797@columbia.edu>
2022-10-05 14:07:23 -0700

    still working on mongodb in Nodejs

--- nosql-mongodb/app.js
+++ nosql-mongodb/app.js
@@ -1,34 +1,33 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const errorController = require('./controllers/error');
+const errorController = require("./controllers/error");
 const mongoConnect = require("./util/database").mongoConnect;
 const User = require("./models/user");
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
   User.findById("633cba5c4d403aa6db3a2a80")
-    .then(user => {
-      req.user = user;
+    .then((user) => {
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
-    .catch(err => console.log(err));
-  next();
+    .catch((err) => console.log(err));
 });
 
-app.use('/admin', adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
